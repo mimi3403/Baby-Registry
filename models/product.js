14009f0c9afe3880e67bf7f46da1845f638a9c97
mimi3403 <miimiixo@gmail.com>
2021-04-21 01:10:54 -0400

    updates products pictures

--- models/product.js
+++ models/product.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+  content: String,
+},{ timestamps: true});
+
 const productSchema = new Schema({
   brand: String,
   productName: String,
   details: String,
   price: Number,
+  reviews: [reviewSchema],
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
