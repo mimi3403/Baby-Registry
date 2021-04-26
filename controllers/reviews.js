a63eda5e317f95623fff3c3c9c487c7995aa6d0b
mimi3403 <miimiixo@gmail.com>
2021-04-25 22:30:04 -0400

    adds home page picture

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -176,11 +176,59 @@ function update(req, res) {
         if(err) return err;
         res.redirect(`/${req.params.category}`)
       })
+    })
+  }
+  if(req.params.category === 'diapers') {
+    Diaper.findById(req.params.id, function(err, item) {  
+      const updatedReview = item.reviews.find(review => review.id === req.params.itemId)
+      updatedReview.content = JSON.stringify(req.body);
+      let data = JSON.parse(updatedReview.content);
+      updatedReview.content = data.reviews;
+      updatedReview.markModified('updatedReview.content')
+      item.save(function(err){
+        if(err) return err;
+        res.redirect(`/${req.params.category}`)
+      })
+    })
+  }
 
-  //     item.reviews.splice(req.body.reviews, 1, updatedReview)
-  //     item.save(function(){
-  //       res.redirect(`/${req.params.category}`);
-  //     })
+  if(req.params.category === 'babygear') {
+    BabyGear.findById(req.params.id, function(err, item) {  
+      const updatedReview = item.reviews.find(review => review.id === req.params.itemId)
+      updatedReview.content = JSON.stringify(req.body);
+      let data = JSON.parse(updatedReview.content);
+      updatedReview.content = data.reviews;
+      updatedReview.markModified('updatedReview.content')
+      item.save(function(err){
+        if(err) return err;
+        res.redirect(`/${req.params.category}`)
+      })
+    })
+  }
+  if(req.params.category === 'baby-must-haves') {
+    BabyMustHave.findById(req.params.id, function(err, item) {  
+      const updatedReview = item.reviews.find(review => review.id === req.params.itemId)
+      updatedReview.content = JSON.stringify(req.body);
+      let data = JSON.parse(updatedReview.content);
+      updatedReview.content = data.reviews;
+      updatedReview.markModified('updatedReview.content')
+      item.save(function(err){
+        if(err) return err;
+        res.redirect(`/${req.params.category}`)
+      })
+    })
+  }
+  if(req.params.category === 'toiletry') {
+    Toiletry.findById(req.params.id, function(err, item) {  
+      const updatedReview = item.reviews.find(review => review.id === req.params.itemId)
+      updatedReview.content = JSON.stringify(req.body);
+      let data = JSON.parse(updatedReview.content);
+      updatedReview.content = data.reviews;
+      updatedReview.markModified('updatedReview.content')
+      item.save(function(err){
+        if(err) return err;
+        res.redirect(`/${req.params.category}`)
+      })
     })
   }
 }
