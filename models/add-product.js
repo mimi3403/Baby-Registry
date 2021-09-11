89b6cae346f8cdc10c23c86f5b74eed5433f8a6c
mimi3403 <miimiixo@gmail.com>
2021-09-11 12:32:59 -0400

    wip: add product

---
+++ models/add-product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  content: String,
+}, { timestamps: true });
+
+const babyGearSchema = new Schema({
+  imageURL: String,
+  brand: String,
+  productName: String,
+  details: String,
+  price: Number,
+  reviews: [reviewSchema],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Baby-Gear', babyGearSchema);
